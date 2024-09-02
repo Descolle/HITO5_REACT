118b9160856a9fb7eef29a4bf14afd0a19e6dcca
Tomas <tomascorvalan14@gmail.com>
2024-09-01 20:59:39 -0400

    faltaba correo

--- src/components/views/Profile.jsx
+++ src/components/views/Profile.jsx
@@ -6,13 +6,12 @@ const Profile = () => {
     <div className='profile_container'>
       <div className='profile'>
         <img src='https://www.shutterstock.com/image-vector/vector-flat-illustration-grayscale-avatar-600nw-2264922221.jpg' alt='Profile Picture' className='profile_pic'></img>
+        <p>correofalso@noexiste.cl</p>
         <button className='profile_btn'>🖋️Edit Profile</button>
-        <li>
-          <ul>Contact</ul>
+          <ul>Info Contact</ul>
           <ul>Address</ul>
           <ul>Orders</ul>
           <ul>Close Session</ul>
-        </li>
       </div>
       <div className='square d-flex justify-content-center align-items-center'>
         <img src='https://www2.udec.cl/~joseparra/2.png' alt='sitio en construcción' className='square_underconstruction'></img>
